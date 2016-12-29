722a85b989c9bf62c1998b6e68981cf942c42d92
mfaithfull <mfaithfull@btopenworld.com>
2016-12-29 22:04:22 +0000

    Beginnings of Windows clang BuildQOR support.

---
+++ Source/BuildQOR/Cmd/clangPPExec.bat
@@ -0,0 +1,19 @@
+IF [%2]==[] (
+rem echo Generating %1.bat from %1.pp
+clang-cl.exe /c /I"%build_shell_path%" /I"..\..\..\include\BuildQOR\Cmd" /I"..\..\..\include\CompilerQOR\Compilers\clang" /I"..\..\..\include" %build_env_params% /W0 /nologo /X /P /EP %1.pp
+echo rem BuildQOR generated script file from %1.pp > %1.bat
+FINDSTR /r "." %1.i >> %1.bat
+del %1.i
+call %1.bat
+del %1.bat
+) else (
+rem echo Generating %1.bat from %2\%1.pp
+copy %2\%1.pp %1.pp >nul
+clang-cl.exe /c /I"%build_shell_path%" /I"..\..\..\include\BuildQOR\Cmd" /I"..\..\..\include\CompilerQOR\Compilers\clang" /I"..\..\..\include" %build_env_params% /W0 /nologo /X /P /EP %1.pp
+echo rem BuildQOR generated script file from %2\%1.pp > %1.bat
+FINDSTR /r "." %1.i >> %1.bat
+del %1.i
+call %1.bat
+del %1.bat
+del %1.pp
+)

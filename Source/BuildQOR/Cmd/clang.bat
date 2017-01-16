079af09e60e0812b5e32fa7b74dc4fb5bfdf54aa
mfaithfull <mfaithfull@btopenworld.com>
2017-01-16 20:52:13 +0000

    clang 4 support

--- Source/BuildQOR/Cmd/clang.bat
+++ Source/BuildQOR/Cmd/clang.bat
@@ -5,4 +5,5 @@ set ppexec=clangPPExec.bat
 set drive=%~dp0
 set build_shell_path=%drive%
 if #%drive:~-1%# == #\# set build_shell_path=%drive:~0,-1%
+echo %ppexec% Projects ..\..\..\Build
 call %ppexec% Projects ..\..\..\Build

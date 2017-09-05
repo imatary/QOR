7ec4a3c3e6677879c75f6da043283d0f12bc447f
mfaithfull <mfaithfull@btopenworld.com>
2017-09-05 20:41:18 +0100

    Removed redundant macro parameter flagged up by clang preprocessor.

--- Source/BuildQOR/Cmd/clangPPExec.bat
+++ Source/BuildQOR/Cmd/clangPPExec.bat
@@ -1,6 +1,6 @@
 IF [%2]==[] (
 echo Generating %1.bat from %1.pp
-d:\tools\LLVM\bin\clang-cl.exe /c /I"%build_shell_path%" /I"..\..\..\include\BuildQOR\Cmd" /I"..\..\..\include\CompilerQOR\Compilers\clang" /I"..\..\..\include" %build_env_params% /W0 /nologo /X /P /EP %1.pp > %1.i
+d:\tools\LLVM\bin\clang-cl.exe /c /I"%build_shell_path%" /I"..\..\..\include\BuildQOR\Cmd" /I"..\..\..\include\CompilerQOR\Compilers\clang" /I"..\..\..\include" %build_env_params% /W0 /nologo /X /P /EP /TP -Wno-invalid-token-paste %1.pp
 echo rem BuildQOR generated script file from %1.pp > %1.bat
 FINDSTR /r "." %1.i >> %1.bat
 del %1.i
@@ -8,8 +8,8 @@ call %1.bat
 del %1.bat
 ) else (
 echo Generating %1.bat from %2\%1.pp
-copy %2\%1.pp %1.pp >nul
-d:\tools\LLVM\bin\clang-cl.exe /c /I"%build_shell_path%" /I"..\..\..\include\BuildQOR\Cmd" /I"..\..\..\include\CompilerQOR\Compilers\clang" /I"..\..\..\include" %build_env_params% /W0 /nologo /X /P /EP %1.pp > %1.i
+copy %2\%1.pp %1.pp
+d:\tools\LLVM\bin\clang-cl.exe /c /I"%build_shell_path%" /I"..\..\..\include\BuildQOR\Cmd" /I"..\..\..\include\CompilerQOR\Compilers\clang" /I"..\..\..\include" %build_env_params% /W0 /nologo /X /P /EP /TP -Wno-invalid-token-paste %1.pp
 echo rem BuildQOR generated script file from %2\%1.pp > %1.bat
 FINDSTR /r "." %1.i >> %1.bat
 rem del %1.i
